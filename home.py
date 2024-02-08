4c3752658791ebbc389ea949ce29089706e10e9b
and0ela <nicholas.roggenkamp@gmail.com>
2024-02-08 14:34:41 -0500

    Update home.py

--- home.py
+++ home.py
@@ -9,7 +9,10 @@ def f(x):
   return ((4*x) - (x**3))
 
 def ff(func,x):
-  return eval(func)
+  try:
+    return eval(func)
+  except:
+    st.warning("Please enter a valid function.")
 
 
 def L(func,lb, rb, nr):
@@ -63,11 +66,14 @@ def T(func,lb, rb, nr):
 
 
 def populate(f,lb,rb,nr):
-  Ln = L(f,lb, rb, nr)
-  Rn = R(f,lb, rb, nr)
-  Mn = M(f,lb, rb, nr)
-  Tn = T(f,lb, rb, nr)
-  return Ln,Rn,Mn,Tn
+  try:
+    Ln = L(f,lb, rb, nr)
+    Rn = R(f,lb, rb, nr)
+    Mn = M(f,lb, rb, nr)
+    Tn = T(f,lb, rb, nr)
+    return Ln,Rn,Mn,Tn
+  except:
+    return None
 
 def main_page():
   st.title("Area-Under-The-Curve Calculator")
@@ -75,6 +81,7 @@ def main_page():
   col1,col2 = st.columns(2)
   with col1:
     func = st.text_input("Please input your function!",value='x' )
+    
     st.caption("Please format your equation using * for multiplication, / for division, and ** for exponents, and use parentheses when necessary!!")
     left_bound = st.number_input("Left Bound")
     right_bound = st.number_input("Right Bound")
@@ -83,104 +90,139 @@ def main_page():
     num_rec = st.number_input("Rectangles",0,None,4,1)
     st.write(func)
     populated = populate(func,left_bound,right_bound,num_rec)
-    st.write(populated)
-    print_vals(populated[0],populated[1],populated[2],populated[3])
+    #st.write(populated)
+    #st.write(type(populated))
+    if populated is not None:
+      print_vals(round(populated[0],4),round(populated[1],4),round(populated[2],4),round(populated[3],4))
 
-    with col2:
-      x1 = []
-      y1 = []
-      data = [x1,y1]
+      with col2:
+        x1 = []
+        y1 = []
+        data = [x1,y1]
 
-      
-      #for i in range(round(left_bound),round(right_bound)+1):
-      i = round(left_bound)
-      while i < round(right_bound):
-        x1.append(i)
-        y1.append(ff(func,i))
-        i = i+0.1
+        
+        #for i in range(round(left_bound),round(right_bound)+1):
+        if left_bound != right_bound:
+          i = round(left_bound)
+          while i < round(right_bound):
+            x1.append(i)
+            y1.append(ff(func,i))
+            i = i+0.1
 
-      fig = px.line(x=x1,y=y1, line_shape="spline")
-      fig.add_hline(y=0, line_color='white',line_width = 0.5) 
-      fig.add_vline(x=0, line_color='white',line_width = 0.5)
-      fig.update_xaxes(showline=True, linewidth=2, linecolor='white')
-      fig.update_yaxes(showline=True, linewidth=2, linecolor='white')
-      plot_spot = st.empty()
+          fig = px.line(x=x1,y=y1, line_shape="spline")
+          fig.add_hline(y=0, line_color='white',line_width = 0.5) 
+          fig.add_vline(x=0, line_color='white',line_width = 0.5)
+          fig.update_xaxes(showline=True, linewidth=2, linecolor='white')
+          fig.update_yaxes(showline=True, linewidth=2, linecolor='white')
+          plot_spot = st.empty()
 
-      #Set up right endpoint boxes
-      
-      
-      
-      #px.histogram(x=x1,y=y1)
-      if st.button("Show Right Endpoints"):
-        interval = ((right_bound - left_bound) / num_rec)
-        leftx = left_bound
-        rightx = left_bound + interval
-        lefty = 0
-        righty = ff(func,rightx)
-        for i in range(num_rec):
-          fig.add_shape(type="rect",
-            x0=leftx,y0=lefty,x1=rightx,y1=righty,
-            line=dict(
-              color="RoyalBlue",
-              width=2,
-            ),
+          interval = ((right_bound - left_bound) / num_rec)
+        #px.histogram(x=x1,y=y1)
+          if st.button("Show Right Endpoints"):
             
-            )
-          leftx = rightx
-          rightx = leftx + interval
-          lefty = 0
-          righty = ff(func,rightx)
+            leftx = left_bound
+            rightx = left_bound + interval
+            lefty = 0
+            righty = ff(func,rightx)
+            for i in range(num_rec):
+              fig.add_shape(type="rect",
+                x0=leftx,y0=lefty,x1=rightx,y1=righty,
+                line=dict(
+                  color="RoyalBlue",
+                  width=2,
+                ),
+                
+                )
+              leftx = rightx
+              rightx = leftx + interval
+              lefty = 0
+              righty = ff(func,rightx)
 
-      if st.button("Show Left Endpoints"):
-        interval = ((right_bound - left_bound) / num_rec)
-        leftx = left_bound
-        rightx = left_bound + interval
-        lefty = 0
-        righty = ff(func,leftx)
-        for i in range(num_rec):
-          fig.add_shape(type="rect",
-            x0=leftx,y0=lefty,x1=rightx,y1=righty,
-            line=dict(
-              color="RoyalBlue",
-              width=2,
-            ),
+          if st.button("Show Left Endpoints"):
+            #interval = ((right_bound - left_bound) / num_rec)
+            leftx = left_bound
+            rightx = left_bound + interval
+            lefty = 0
+            righty = ff(func,leftx)
+            for i in range(num_rec):
+              fig.add_shape(type="rect",
+                x0=leftx,y0=lefty,x1=rightx,y1=righty,
+                line=dict(
+                  color="RoyalBlue",
+                  width=2,
+                ),
+                
+                )
+              leftx = rightx
+              rightx = leftx + interval
+              lefty = 0
+              righty = ff(func,leftx)
+
+
+          if st.button("Show Midpoints"):
+            #interval = ((right_bound - left_bound) / num_rec)
+            leftx = left_bound
+            rightx = left_bound + interval
+            lefty = 0
+            righty = ff(func,(leftx+rightx)/2)
+            for i in range(num_rec):
+              fig.add_shape(type="rect",
+                x0=leftx,y0=lefty,x1=rightx,y1=righty,
+                line=dict(
+                  color="RoyalBlue",
+                  width=2,
+                ),
+                
+                )
+              leftx = rightx
+              rightx = leftx + interval
+              lefty = 0
+              righty = ff(func,(leftx+rightx)/2)
+
+          if st.button("Show trapezoids"):
+            trapx = []
+            trapy = []
+            leftx = left_bound
+            rightx = left_bound + interval
+            lefty = ff(func,leftx)
+            righty = ff(func,rightx)
+            trapx.extend((leftx,leftx,rightx,rightx))
+            trapy.extend((0,lefty,righty,0))
+            fig.add_trace(go.Scatter(x=trapx,y=trapy))
+            for i in range(num_rec):
+              pass
+
+            '''''
+            trapx = []
+            trapy = []
+            leftx = left_bound
+            rightx = left_bound + interval
+            lefty = ff(func,leftx)
+            righty = ff(func,rightx)
+            trapx.extend((leftx,leftx,rightx,rightx))
+            trapy.extend((0,lefty,righty,0))
+            for i in range(num_rec*2):
+              fig.add_trace(go.Scatter(x=trapx,y=trapy))
+              trapx.extend("None")
+              leftx = rightx
+              rightx = leftx + interval
+              lefty = ff(func,leftx)
+              righty = ff(func,rightx)
+              trapx.extend((leftx,leftx,rightx,rightx))
+              trapy.extend((0,lefty,righty,0))
+              '''''
             
-            )
-          leftx = rightx
-          rightx = leftx + interval
-          lefty = 0
-          righty = ff(func,leftx)
 
-
-      if st.button("Show Midpoints"):
-        interval = ((right_bound - left_bound) / num_rec)
-        leftx = left_bound
-        rightx = left_bound + interval
-        lefty = 0
-        righty = ff(func,(leftx+rightx)/2)
-        for i in range(num_rec):
-          fig.add_shape(type="rect",
-            x0=leftx,y0=lefty,x1=rightx,y1=righty,
-            line=dict(
-              color="RoyalBlue",
-              width=2,
-            ),
-            
-            )
-          leftx = rightx
-          rightx = leftx + interval
-          lefty = 0
-          righty = ff(func,(leftx+rightx)/2)
-
-      with plot_spot:
-        st.plotly_chart(fig)
-      #st.write(x1)
-      #st.write(y1)
+          with plot_spot:
+            st.plotly_chart(fig)
+          #st.write(x1)
+          #st.write(y1)
 
 
 
 
 def print_vals (Ln,Rn,Mn,Tn):
+
   st.write("L: ", Ln)
   st.write("R: ", Rn)
   st.write("M: ", Mn)
